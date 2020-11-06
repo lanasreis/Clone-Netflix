a5eb75d3c0cc965f9904f97a11625483241efe1e
lanasreis <lanareissdd@gmail.com>
2020-11-06 19:17:11 -0300

    Finalizado, adicionai botão de vídeo interativo, além de alterar questões estéticas como fonte de letra e logo Netflix

---
+++ js/script.js
@@ -0,0 +1,56 @@
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items:4,
+    loop:true,
+    margin:10,
+    autoplay:true,
+    autoplayTimeout:10000,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:4
+        },
+        1000:{
+            items:8
+        }
+    }
+});
+$('.play').on('click',function(){
+    owl.trigger('play.owl.autoplay',[1000])
+})
+$('.stop').on('click',function(){
+    owl.trigger('stop.owl.autoplay')
+})
+
+$(function(){
+
+    // fixar header
+    $(window).scroll(function(){
+        if($(this).scrollTop() > 80){
+            $('.header').addClass('fixed');
+        }
+        else{
+            $('.header').removeClass('fixed');
+        }
+    })
+
+    //video
+    $('.botao').click(function(e) {
+        e.preventDefault();
+        $('.video').addClass('active');
+    })
+
+    //esconde vídeo
+    $(document).on("click", function(e){
+        if($(e.target).closest(".eu")[0] || $(e.target).closest(".botao")[0] ){
+           e.stopPropagation();
+        }else{
+            $('.video').removeClass('active');
+            location.reload();
+        }
+     });
+   
+});
